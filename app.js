eef3a0a4ad0f8f1921e589027867e8083a0d6ac6
Daniel Reza <daniel.reza1007@gmail.com>
2021-01-05 22:03:13 -0600

    linking first query

--- app.js
+++ app.js
@@ -3,22 +3,27 @@ const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-
   port: 3306,
-
   user: 'root',
-
   password: 'drum1007',
-
   database: 'employees_mainDB',
 });
 
 connection.connect((err) => {
   if (err) throw err;
   console.log('connected as id ' + connection.threadId);
+  afterConnection();
 //   songSearch();
 //   artistSearch("bing crosby");
 //   songSearch("my heart will go on");
 //   songAndAlbumSearch("the beatles");
-  connection.end();
 });
+
+function afterConnection() {
+
+    connection.query("SELECT * FROM employees;", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end(); //needed to end in last function
+    });
+};
---
+++ employeeSeeds.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employees_mainDB;
+
+CREATE database employees_mainDB;
+
+USE employees_mainDB;
+
+CREATE TABLE employees (
+    employee_id INTEGER AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    role_id INTEGER (11),
+    manager_id INTEGER (11) NULL,
+    PRIMARY KEY (employee_id)
+);
+
+CREATE TABLE role (
+    role_id INTEGER AUTO_INCREMENT NOT NULL,
+    title VARCHAR (30),
+    salary DECIMAL (10, 2),
+    department_id INTEGER (11),
+    PRIMARY KEY (role_id)
+);
+
+CREATE TABLE department (
+    department_id INTEGER AUTO_INCREMENT,
+    department_name VARCHAR (30),
+    PRIMARY KEY (department_id)
+);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Daniel", "Reza", 12345, 00000), ("Kaylee", "Reza", 54321, 00000);
